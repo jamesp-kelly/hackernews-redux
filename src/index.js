018708e4beb3c3924225bbd9c9342546174d73b1
James Kelly <jk_jameskelly@outlook.com>
2017-04-16 13:20:15 -0400

    initial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
+
+const tracks = [
+  {
+    title: 'some track'
+  },
+  {
+    title: 'some other track'
+  }
+];
+
+ReactDOM.render(
+  <Stream tracks={tracks} />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
